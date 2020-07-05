52505d16e434dfb3cc1548e980530f47d805114f
Juan Diego Lozano <33541161+jdlozanom@users.noreply.github.com>
2020-07-05 23:20:16 +0200

    Initial commit

---
+++ app/src/main/java/com/mlearners/cameratoserver/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mlearners.cameratoserver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
